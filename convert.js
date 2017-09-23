89f5938497d01b4f31ddf5c63be80b25c45e98bb
Rainer Stropek <rainer@timecockpit.com>
2017-09-23 13:11:31 +0200

    Add some tips

--- convert.js
+++ convert.js
@@ -1,3 +1,5 @@
+// You could use the standard function Array.find() instead.
+// Learn more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 function Contains(array, value){
     for(var i = 0; i < array.length; i++){
         if(array[i] === value)return true;
@@ -8,12 +10,15 @@ function HandleError(){
     console.log('Invalid parameters');
     process.exit(-1);
 }
+// If you would add the factors to the following data structure, your if/switch-code below could be simplified
 var legalUnits = ['m', 'cm', 'mm', 'kg', 'g'];
+// Tip: Format code using VSCode's format feature. Would add some whitespace that makes code more readable.
 if(process.argv.length !== 6)HandleError();
 if(isNaN(process.argv[2]))HandleError();
 if(process.argv[4] !== 'to')HandleError(); 
 if(!Contains(legalUnits, process.argv[3]))HandleError();
 if(!Contains(legalUnits, process.argv[5]))HandleError();
+// Smart solution ;-) Not generally usable but fine for this example.
 if(String(process.argv[3]).charAt(process.argv[3].length - 1) !== String(process.argv[5]).charAt(process.argv[5].length - 1))HandleError();
 
 var floatInput = parseFloat(process.argv[2]);
